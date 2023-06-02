63e6230bb5f9375c8a4988b2d7c6dfaee7de77bc
Kyle Kemp <kyle@seiyria.com>
2023-06-02 11:23:34 -0500

    make pwa, add some boilerplate, etc

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,46 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsStoragePluginModule, StorageOption } from '@ngxs/storage-plugin';
+import { NgxsModule } from '@ngxs/store';
 
-import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+const Migrations: Record<any, any> = {};
+const allStores: any[] = [];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    NgxsModule.forRoot(allStores, {
+      developmentMode: !isDevMode()
+    }),
+    NgxsLoggerPluginModule.forRoot({
+      disabled: !isDevMode(),
+      filter: action => !action.constructor.name.includes('Timer')
+    }),
+    NgxsStoragePluginModule.forRoot({
+      key: allStores,
+      migrations: Object.values(Migrations).flat(),
+      storage: StorageOption.LocalStorage
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
