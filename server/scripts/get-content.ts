6d159e8858f95cc8d0b6df075124e7ea1bba1fc0
Kyle Kemp <kyle@seiyria.com>
2023-06-13 17:00:23 -0500

    http fallback for loading content

--- server/scripts/get-content.ts
+++ server/scripts/get-content.ts
@@ -11,4 +11,4 @@ const getData = async () => {
   }
 };
 
-getData();
+void getData();
--- server/src/modules/content/content.service.ts
+++ server/src/modules/content/content.service.ts
@@ -1,15 +1,43 @@
 import { Injectable } from '@nestjs/common';
 
 import { ILocation } from '@interfaces';
-import * as content from '../../../content.json';
+import * as fs from 'fs-extra';
+import { Logger } from 'nestjs-pino';
 
 @Injectable()
 export class ContentService {
-  public get content() {
-    return (content as any).default || content;
-  }
+  public content = { locations: {} };
 
   public get locations(): Record<string, ILocation> {
     return this.content.locations;
+  }
+
+  constructor(private logger: Logger) {}
+
+  public async reloadContent() {
+    this.logger.log('Loading game content over HTTP...');
+
+    try {
+      const data = await fetch('https://content.ateoat.com/content.json');
+      const json = await data.json();
+
+      this.content = json;
+
+      // this isn't relevant in the game server context, but is useful in case someone forgets to run setup
+      fs.writeJsonSync('content.json', json);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  public async onModuleInit() {
+    if (fs.existsSync('content.json')) {
+      this.logger.log('Content exists locally; loading from file.');
+      const content = fs.readJsonSync('content.json');
+      this.content = content;
+      return;
+    }
+
+    await this.reloadContent();
   }
 }
