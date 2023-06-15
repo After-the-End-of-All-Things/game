d175bf6be0a9f09cdc141ffd913d5cc60a7f9554
Kyle Kemp <kyle@seiyria.com>
2023-06-14 22:13:48 -0500

    ignore auth/register in update time interceptor

--- server/src/utils/updatetime.interceptor.ts
+++ server/src/utils/updatetime.interceptor.ts
@@ -16,7 +16,7 @@ export class UpdateAuthTimeInterceptor implements NestInterceptor {
 
   async intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
-    const ignoredRoutes = ['/auth/login', '/user/online'];
+    const ignoredRoutes = ['/auth/login', '/auth/register', '/user/online'];
 
     if (
       request.headers.authorization &&
