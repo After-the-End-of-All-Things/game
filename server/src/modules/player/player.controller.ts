aff4bd1ada053cc607abde3c13e27bc987ecc0c4
sarimash <brandon.stickle@gmail.com>
2023-06-24 01:10:38 -0700

    Block changing portrait if the user has not unlocked it

--- server/src/modules/player/player.controller.ts
+++ server/src/modules/player/player.controller.ts
@@ -4,20 +4,39 @@ import { PlayerService } from '@modules/player/player.service';
 import { Body, Controller, Patch, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { User } from '@utils/user.decorator';
+import { DiscoveriesService } from '@modules/discoveries/discoveries.service';
+import { NotFoundException } from '@nestjs/common';
 
 @ApiBearerAuth()
 @Controller('player')
 export class PlayerController {
-  constructor(private readonly playerService: PlayerService) {}
+  constructor(
+    private readonly playerService: PlayerService,
+    private readonly discoveriesService: DiscoveriesService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
-  @ApiOperation({ summary: 'Get the online users count' })
+  @ApiOperation({ summary: 'Change Player Portrait' })
   @Patch('cosmetics/portrait')
   async changePortrait(
     @User() user,
     @Body('portrait') portrait: number,
   ): Promise<Partial<IFullUser | IPatchUser>> {
     const portraitId = Math.round(Math.min(107, Math.max(0, portrait)));
+
+    // Fetch the player's discoveries
+    const discoveries = await this.discoveriesService.getDiscoveriesForUser(
+      user.userId,
+    );
+
+    if (!discoveries) {
+      throw new NotFoundException('Discoveries not found for this user.');
+    }
+
+    // Check if the portrait is unlocked
+    if (!discoveries.portraits[portraitId.toString()]) {
+      throw new NotFoundException(`Portrait ${portraitId} is not unlocked.`);
+    }
 
     return this.playerService.updatePortraitForPlayer(user.userId, portraitId);
   }
