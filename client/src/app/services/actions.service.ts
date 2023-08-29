6293a4f49bb8be0407e8aea6660e556ad704b146
Kyle Kemp <kyle@seiyria.com>
2023-08-28 22:50:01 -0500

    fix(notifications): notifications with actions will no longer race to submit data and clear the actions

--- client/src/app/services/actions.service.ts
+++ client/src/app/services/actions.service.ts
@@ -28,18 +28,24 @@ export class ActionsService {
   doAction(action: INotificationAction, notification?: INotification) {
     if (action.url) {
       // we never send data here, it's inferred server side
-      this.http.post(`${environment.apiUrl}/${action.url}`, {}).subscribe();
+      this.http
+        .post(`${environment.apiUrl}/${action.url}`, {})
+        .subscribe(() => {
+          if (!notification) return;
+
+          this.notificationService
+            .clearActions(notification.id || '')
+            .subscribe();
+          this.store.dispatch(
+            new ClearNotificationActions(notification.id || ''),
+          );
+        });
     }
 
     if (action.clearActionsForUrl) {
       this.store.dispatch(
         new ClearNotificationActionsMatchingUrl(action.clearActionsForUrl),
       );
-    }
-
-    if (notification) {
-      this.notificationService.clearActions(notification.id || '').subscribe();
-      this.store.dispatch(new ClearNotificationActions(notification.id || ''));
     }
 
     if (action.action === 'navigate') {
