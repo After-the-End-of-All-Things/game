42e669c97389cd6adddb6a9cec94550978331d82
Kyle Kemp <kyle@seiyria.com>
2023-08-29 07:18:13 -0500

    feat(updates): blogpost generator puts version in title

--- utils/blog-version-notes.ts
+++ utils/blog-version-notes.ts
@@ -1,4 +1,6 @@
-export { };
+export {};
+
+const packageJSON = require('../package.json');
 
 const fs = require('fs-extra');
 
@@ -12,7 +14,7 @@ const minutes = date.getMinutes().toString().padStart(2, '0');
 const seconds = date.getSeconds().toString().padStart(2, '0');
 
 const header = `---
-title: Version Updates
+title: Version Updates - ${packageJSON.version}
 date: ${year}-${month}-${day} ${hours}:${minutes}:${seconds}
 categories: [New Version]
 tags: [ateoat, version-notes]
