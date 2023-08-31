32f794400e38b9a84d18aee1fcb5a54433df41bb
Kyle Kemp <kyle@seiyria.com>
2023-08-31 07:55:52 -0500

    fix(core): hopefully add better error handling. closes #133

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,11 +1,37 @@
 import { UserResponse } from '@interfaces';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 import { fromEvent } from 'rxjs';
+
+import { ROLLBAR_CONFIG } from '@modules/config/rollbar';
+import { Logger } from 'nestjs-pino';
+import * as Rollbar from 'rollbar';
 
 @Injectable()
 export class AppService {
   private readonly events = new EventEmitter2();
+  private rollbar: Rollbar;
+
+  constructor(
+    private logger: Logger,
+    @Inject(ROLLBAR_CONFIG) private rollbarConfig,
+  ) {
+    this.rollbar = new Rollbar(this.rollbarConfig);
+
+    this.watchForUncaughtErrors();
+  }
+
+  private watchForUncaughtErrors() {
+    process.on('uncaughtException', (error: Error) => {
+      this.logger.error(error);
+      this.rollbar.error(error);
+    });
+
+    process.on('unhandledRejection', (error: Error) => {
+      this.logger.error(error);
+      this.rollbar.error(error);
+    });
+  }
 
   public subscribe(channel: string) {
     return fromEvent(this.events, channel);
--- server/src/utils/http-exception.filter.ts
+++ server/src/utils/http-exception.filter.ts
@@ -1,23 +1,13 @@
-import { ROLLBAR_CONFIG } from '@modules/config/rollbar';
 import {
   ArgumentsHost,
   Catch,
   ExceptionFilter,
   HttpException,
-  Inject,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-import * as Rollbar from 'rollbar';
-
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  private rollbar: Rollbar;
-
-  constructor(@Inject(ROLLBAR_CONFIG) private rollbarConfig) {
-    this.rollbar = new Rollbar(this.rollbarConfig);
-  }
-
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
